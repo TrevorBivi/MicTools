e09a6513bf5ff1c9f3be3e7715b72a87ad3840cc
Trevor Bivi <TrevorBivi@cmail.carleton.ca>
2019-02-07 23:21:02 -0500

    Began adding python structure for handling events reported over serial

--- serialController.py
+++ serialController.py
@@ -1,6 +1,53 @@
 import serial
 
 ser = serial.Serial('COM1',9600)
+
+class Button():
+    def __init__(self,downFunc=None,upFunc=None,isPressed=False):
+        self.isPressed = isPressed
+        self.downFunc = downFunc
+        self.upFunc = upFunc
+
+class Analog():
+    def __init__(self, minVal, maxVal, changeFunc):
+        self.maxVal = maxVal
+        self.minVal = minVal
+        self.changeFunc = changeFunc
+
+class MultiPurposeAnalog(Analog):
+    def __init__(self, minVal, MaxVal, changeFunc=None, funcChangeFunc=None,):
+        Analog.__init__(minVal,maxVal,changeFunc)
+        self.funcChangeFunc = funcChangeFunc
+
+def selectSong(pos):
+    newSong = selectedPlaylist[pos]
+    if selectedSong != newSong:
+        tts(newSong.name)
+
+######
+
+def changeSong(val,minVal,maxVal):
+    global selectedSong
+    vPerSong = (maxVal - minVal) / len(selectedPlaylist)
+    song = (val - minVal) // vPerSong
+    selectedSong = 
+
+#####
+
+mpAnalog = MultiPurposeAnalog()
+
+
+buttons = [
+    Button(
+        downFunc = (lambda : )
+        ),
+
+    ]
+
+analogs = [
+    mpAnalog
+    ]
+
 while True:
     try:
         val = ser.read()
