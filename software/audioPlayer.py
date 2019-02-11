69e71ca52fa7142ff0bad711bdba3db107ff51e3
Trevor Bivi <trevorbivi@cmail.carleton.ca>
2019-02-11 00:03:36 -0500

    working audioPlayer

--- software/audioPlayer.py
+++ software/audioPlayer.py
@@ -10,67 +10,83 @@ class CD():
     def __init__(self,path):
         self.name = path.split('\\')[-1]
         self.songs = []
-        for songPath in os.listdir(path):#[p[0] for p in os.walk(path)][1:]:
-            self.songs.append(song(path + '\\' + songPath))
-
+        for songPath in os.listdir(path):
 class audioPlayer():
     def __init__(self,audioPath):
         self.CDs = []
-        for CDPath in os.listdir(audioPath):#[p[0] for p in os.walk(audioPath)][1:]:
+        for CDPath in os.listdir(audioPath):
             self.CDs.append( CD(audioPath + '\\' + CDPath) )
         self.CDIndex = None
         self.songIndex = None
         self.volume = 100
+        self.player = vlc.MediaPlayer()
 
-    def nextCD():
+    def getSelectedSong(self):
+        if self.CDIndex == None or self.songIndex == None:
+            return None
+        CD = self.CDs[self.CDIndex]
+        song = CD.songs[self.songIndex]
+        return song
+
+    def playSong(self):
+        self.player.play()
+
+    def pauseSong(self):
+        self.player.pause()
+
+    def stopSong(self):
+        self.player.stop()
+
+    def setVolume(self, volume):
+        self.volume = volume
+        self.player.audio_set_volume(self.volume)
+
+    def nextCD(self):
         if self.CDIndex == None or self.CDIndex == len(self.CDs)-1:
-            self.CDIndex = 0
+            self.setCD(0)
         else:
-            self.CDIndex += 1
-        self.songIndex = None
+            self.setCD(self.CDIndex + 1)
 
-    def prevCD():
+    def prevCD(self):
         if self.CDIndex == None or self.CDIndex == 0:
-            self.CDIndex = len(self.CDs)-1
+            self.setCD(len(self.CDs) - 1)
         else:
-            self.CDIndex -= 1
-        self.songIndex = None
+            self.setCD(self.CDIndex - 1)
 
-    def setCD(index)
+    def setCD(self,index):
         self.CDIndex = index
+        self.songIndex = None
+        self.player.set_media(None)
 
-    def getCDCount():
+    def getCDCount(self):
         return len(self.CDs)
 
-    def nextSong():
-        songList = self.CDs[self.CDIndex]
-        if self.songIndex == None or self.songIndex == len(songList)-1:
-            self.songIndex = 0
-        else:
-            self.songIndex += 1
+    def nextSong(self):
+        if self.CDIndex != None:
+            songList = self.CDs[self.CDIndex].songs
+            if self.songIndex == None or self.songIndex == len(songList)-1:
+                self.setSong(0)
+            else:
+                self.setSong(self.songIndex + 1)
 
-    def prevSong():
+    def prevSong(self):
         if self.songIndex == None or self.songIndex == 0:
-            songList = self.CDs[self.CDIndex]
-            self.songIndex = len(songList)-1
+            songList = self.CDs[self.CDIndex].songs
+            self.setSong(len(songList) - 1)
         else:
-            self.songIndex -= 1
+            self.setSong(self.songIndex - 1)
 
-    def setSong(index):
+    def setSong(self,index):
         self.songIndex = index
+        song = self.getSelectedSong()
+        if song:
+            self.player.set_media( vlc.Media( song.path ) )
 
-    def getSongCount():
+    def getSongCount(self):
         if self.CDIndex:
             return len(self.CDs[self.CDIndex])
+        return None
         
             
     
 ap = audioPlayer("C:\\Users\\Trevor\\Music\\VR CDS")
-
-'''
-        player = vlc.MediaPlayer("C:\\Users\\Trevor\\Dropbox\\Mic Spams\\white farts - chant.mp3")
-        player.audio_set_volume(100)
-        player.play()
-        self.player = player
-
-'''
