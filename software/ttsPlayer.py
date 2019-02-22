6149800a61cf58a67368a2725cc24dc67f59df3a
Trevor Bivi <TrevorBivi@cmail.carleton.ca>
2019-02-21 20:22:00 -0500

    improved

--- software/ttsPlayer.py
+++ software/ttsPlayer.py
@@ -4,12 +4,15 @@ si = subprocess.STARTUPINFO()
 si.dwFlags |= subprocess.STARTF_USESHOWWINDOW
 
 class Balcon(object):
-    def __init__(self, path='C:\\Users\\Trevor\\Documents\\MicTools\\balcon\\balcon.exe', volume=100, speed=5, device=''):
+    def __init__(self, path='..\\balcon\\balcon.exe', volume=100, speed=5, device=None):
         self.path = path
         self.volume = volume
         self.speed = speed
         self.device = device
 
     def say(self,text,cutoff=False):
-        cmd_inp = self.path +' -s ' + str(self.speed) + ' -v ' + str(self.volume) + ' -t "' + text + '"'
+        cmd_inp = self.path +' -s ' + str(self.speed) + ' -v ' + str(self.volume)
+        if self.device:
+            cmp_inp += ' -r ' + device
+        cmd_inp += ' -t "' + text + '"'
         subprocess.call(cmd_inp, startupinfo=si)
